1e05b2e076838eac5718cbc32f9843ddbaf58a56
macha <fede-cabj@live.com.ar>
2023-05-23 18:53:29 -0300

    mejora

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -10,29 +10,29 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
   constructor(private readonly messagesWsService: MessagesWsService) { }
 
   handleConnection(client: Socket) {
-    this.messagesWsService.registerClient(client)
-    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients())
+    this.messagesWsService.registerClient(client);
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
+    this.wss.emit('message-from-server', this.messagesWsService.getEstimations());
   }
   handleDisconnect(client: Socket) {
     this.messagesWsService.removeClient(client.handshake.headers.user as string);
-    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients())
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
   }
   @SubscribeMessage('message-from-client')
   onMessageFromClient(client: Socket, payload: NewMessageDto) {
     this.messagesWsService.registerEstimation({ name: client.handshake.headers.user as string, point: payload.message })
 
-    this.wss.emit('message-from-server', this.messagesWsService.getEstimations())
+    this.wss.emit('message-from-server', this.messagesWsService.getEstimations());
 
   }
   @SubscribeMessage('message-from-client2')
-
   onMessageFromClient2(client: Socket, payload: any) {
 
     if (payload.mostrar == true) {
-      this.wss.emit('message-from-server2', true)
+      this.wss.emit('message-from-server2', true);
     } else {
-      this.messagesWsService.cleanEstimations()
-      this.wss.emit('message-from-server2', false)
+      this.messagesWsService.cleanEstimations();
+      this.wss.emit('message-from-server2', false);
 
 
     }
