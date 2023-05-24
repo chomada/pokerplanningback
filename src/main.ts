3bd198603390b3dab8b2c14ddcb2fc5d540cacc5
macha <fede-cabj@live.com.ar>
2023-05-24 07:42:32 -0300

    cors again

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: [
+      'http://localhost:3000',
+      'https://pypaplanning.netlify.app/',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
