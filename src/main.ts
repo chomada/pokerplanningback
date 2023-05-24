1b61564e375644dfa97bd27683aa2d80a2807f3f
macha <fede-cabj@live.com.ar>
2023-05-24 08:04:31 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,12 @@ async function bootstrap() {
     origin: ['http://localhost:3000','https://pypaplanning.netlify.app/'],
     credentials: true,
   });
-  
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   await app.listen(3001);
 }
 bootstrap();
