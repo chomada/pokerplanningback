e78c7619c6676065f0548fe127b4ba8d35338310
macha <fede-cabj@live.com.ar>
2023-05-24 08:29:30 -0300

    vercel cors

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors:true});
- const cors = require('cors');
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  const corsOptions: CorsOptions = {
-    allowedHeaders: ['origin', 'x-requested-with', 'content-type', 'accept', 'authorization'],
-    credentials: true,
-    origin: ['https://pypaplanning.netlify.app', 'http://localhost:3000'],
-  }
-  app.enableCors(corsOptions);
-  app.use(cors());
-
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3001);
 }
 bootstrap();
